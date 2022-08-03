d52cdc1f13896ea33d1b29b82a82425c03cef000
Salabombo <salabombo@outlook.com>
2022-08-03 11:08:49 +0300

    now there are two lists, one visible to end user and one hidden to store past 1000 songs

--- cogs/music.py
+++ cogs/music.py
@@ -1,4 +1,5 @@
 
+from turtle import dot
 from discord.ext import commands
 import discord
 import asyncio
@@ -18,6 +19,13 @@ looping = {}
 def get_server(ctx):
     return str(ctx.message.guild.id)
 
+def append_songs(ctx, songs=[]): # appends songs to the playlist
+    server = get_server(ctx)
+    length = len(playlist[server][0])
+    playlist[server][1] += songs
+    playlist[server][0] = playlist[server][0] +  playlist[server][1][0:1000 - length] # limits the visible playlist to go to upto 1000 song at once
+    del playlist[server][1][0:1000 - length]
+
 def get_duration(youtube, videoId): # youtube api v3 needs a v4
     request = youtube.videos().list(
         part='contentDetails',
@@ -32,40 +40,40 @@ def get_duration(youtube, videoId): # youtube api v3 needs a v4
     return duration.seconds # returns the duration of the song that was not included in the snippet for some reason
 
 def serialize_songs(server):
-    length = len(str(len(playlist[server]))) #cursed cast. todo fix whatever the fuck this is # it just works trust me bro
+    length = len(str(len(playlist[server][0][0:101]))) #cursed cast. todo fix whatever the fuck this is # it just works trust me bro
     i = 0
-    array = []
-    for song in playlist[server]:
-        dots = ' ...'
+    songs = []
+    for song in playlist[server][0]:
         digit = f'{i}'.zfill(length)
-        if not len(song.title) > 34:
-            dots = ''
-        song = f"```{digit}: {song.title}"[0:44] + f'{dots}```'
-        array.append(song)
+        title = song.title[0:30]
+        title_length = len(title)
+        if title_length == 30 :
+            title += ' ...'
+        song = f'**{digit}**: {title}'
+        songs.append(song)
         i += 1
-    if len(array) <= 1:
+    if len(songs) <= 1:
         return ['']
-    array.pop(0)
-    return array
+    songs.pop(0)
+    return songs
 
 def create_embed(ctx, page_num): # todo add timestamp
     server = get_server(ctx)
     embed = discord.Embed(title='PLAYLIST', description='', fields=[])
-    index = page_num * 25
+    index = page_num * 50
     songs = serialize_songs(server)
-    if playlist[server] != []:
-        currently_playing = playlist[server][0]
-    for song in songs[index:25 + index][::-1]:
-        embed.description += song# + '\n'
+    if playlist[server][0] != []:
+        currently_playing = playlist[server][0][0]
+    for song in songs[index:50 + index][::-1]:
+        embed.description += song + '\n'
     embed.add_field(name='CURRENTLY PLAYING:', value=f'```{currently_playing.title}```')
     return embed
 
 def create_options(ctx):
     server = get_server(ctx)
-    page_amount = math.ceil(len(playlist[server]) / 25)
+    page_amount = math.ceil(len(playlist[server][0]) / 50)
     options = []
     for i in range(0, page_amount):
-        if i >= 25: break
         options.append(
             discord.SelectOption(
                 label=f'Page {i+1}',
@@ -90,12 +98,12 @@ class music(commands.Cog):
         if before.channel != None:
             if len(before.channel.members) == 1:
                 server = str(before.channel.guild.id)
-                playlist[server] = []
+                playlist[server] = [[],[]]
                 await before.channel.guild.voice_client.disconnect()
     def create_info_embed(self, ctx, number='0', song=None):
         server = get_server(ctx)
         if song == None:
-            song = playlist[server][abs(int(number))]
+            song = playlist[server][0][abs(int(number))]
         embed = discord.Embed(title=f'{song.title} - {song.channel}', description=song.description, fields=[])
         embed.set_image(url=song.thumbnail)
         return embed
@@ -115,7 +123,6 @@ class music(commands.Cog):
         elif 'list' in query:
             song = await YouTube.fetch_from_playlist(self.youtube, playlistId=query['list'][0])
             async def fetch_the_rest():
-                server = get_server(ctx)
                 await asyncio.sleep(0.5) # just incase
                 request = self.youtube.playlistItems().list(
                     part='snippet',
@@ -128,22 +135,21 @@ class music(commands.Cog):
                     items += r['items']
                     request = self.youtube.playlistItems().list_next(request, r)
                 songs = []
-                for song in items:
+                for song in items[1:]:
                     song = song['snippet']
                     songs.append(YouTube.Video(data=song))
-                playlist[server] += songs[1:]
+                append_songs(ctx, songs)
             asyncio.ensure_future(fetch_the_rest()) # fire and forget
             return [song]
         raise Exception('Invalid url')
 
-    def play_song(self, ctx, songs=None):
+    def play_song(self, ctx, songs=[]):
         if ctx.voice_client == None:
             return
         server = get_server(ctx)
-        if songs != None:
-            playlist[server] += songs
+        append_songs(ctx, songs)
         if not ctx.voice_client.is_playing() and playlist[server] != []:
-            song = playlist[server][0]
+            song = playlist[server][0][0]
             url = YouTube.get_raw_audio_url(f"https://www.youtube.com/watch?v={song.id}")
             source = discord.FFmpegPCMAudio(url, **self.ffmpeg_options)
             embed = self.create_info_embed(ctx)
@@ -155,9 +161,9 @@ class music(commands.Cog):
         try:
             asyncio.run_coroutine_threadsafe(message.delete(), self.bot.loop)
         except: pass # incase the message was deleted or something so it wont fuck up the whole queue
-        if playlist[server] != []:
+        if playlist[server][0] != []:
             if not looping[server]:
-                playlist[server].pop(0)
+                playlist[server][0].pop(0)
             self.play_song(ctx)        
 
     @commands.command()
@@ -169,7 +175,7 @@ class music(commands.Cog):
         await VoiceChat.join(ctx)
         songs = await self.fetch_songs(ctx, url, args)
         if not server in playlist:
-                playlist[server] = []
+                playlist[server] = [[],[]]
                 looping[server] = False
         await ctx.message.add_reaction('👌')
         self.play_song(ctx, songs)
@@ -188,12 +194,12 @@ class music(commands.Cog):
     @commands.command()
     async def link(ctx):
         server = get_server(ctx)
-        linked_message = await ctx.send("Currently playing " + str(playlist[server][0].title + "\n" + f"https://www.youtube.com/watch?v={playlist[server][0].id}"),delete_after=30)
+        linked_message = await ctx.send("Currently playing " + str(playlist[server][0][0].title + "\n" + f"https://www.youtube.com/watch?v={playlist[server][0][0].id}"),delete_after=30)
     @commands.command()
     async def disconnect(self, ctx):
         if ctx.voice_client == None: return
         server = get_server(ctx)
-        playlist[server] = []
+        playlist[server] = [[],[]]
         await VoiceChat.leave(ctx)
         await ctx.message.add_reaction('👌')
 
@@ -217,14 +223,18 @@ class music(commands.Cog):
         server = get_server(ctx)
         temp = looping[server]
         looping[server] = False
-        for _ in range(1, abs(int(amount))): 
-            try:
-                playlist[server].pop(0)
-            except:
+        amount = abs(int(amount))
+        for _ in range(1, amount): 
+            if playlist[server][0] != []:
+                playlist[server][0].pop(0)
+            elif playlist[server][1] != []:
+                playlist[server][1].pop(0)
+            else:
                 break
+        append_songs(ctx)
         await VoiceChat.stop(ctx) # skips one song
         await asyncio.sleep(0.5) #why? # just incase
-        # also why do we not refresh the list after this. todo 
+        # also why do we not refresh the list after this. todo # done ;)
         looping[server] = temp
         await ctx.message.add_reaction('👌')
     
@@ -232,12 +242,12 @@ class music(commands.Cog):
     async def shuffle(self, ctx):
         if ctx.voice_client == None: return
         server = get_server(ctx)
-        if playlist[server] == []: return
-        temp = playlist[server][0]
-        playlist[server].pop(0)
-        await asyncio.sleep(3) # incase the playlist hasnt been appended to playlist[server]
-        np.random.shuffle(playlist[server])
-        playlist[server].insert(0, temp)
+        if playlist[server] == [[],[]]: return
+        temp = playlist[server][0][0]
+        playlist[server][0].pop(0)
+        np.random.shuffle(playlist[server][0])
+        np.random.shuffle(playlist[server][1])
+        playlist[server][0].insert(0, temp)
         await ctx.message.add_reaction('👌')
 
     @commands.command()
@@ -273,7 +283,8 @@ class music_view(discord.ui.View):
         await interaction.response.edit_message(view=self)
 
     def update_buttons(self): # holy fuck
-        playlist_length = math.ceil(len(playlist[self.server]) / 25)
+        self.children[0].options = create_options(self.ctx)
+        playlist_length = math.ceil(len(playlist[self.server][0]) / 50)
 
         for i in range(0, len(self.children)):
             self.children[i].disabled = False
@@ -284,18 +295,24 @@ class music_view(discord.ui.View):
         if self.index >= playlist_length - 1: # no more to forward
             self.children[4].disabled = True # for
             self.children[5].disabled = True # super for
-        if playlist[self.server] == []: # if the entire list is empty
+        if playlist[self.server][0] == []: # if the entire list is empty
             for i in range(0, len(self.children)):
                 self.children[i].disabled = True
             self.children[3].disabled = False # refresh
             self.index = 0
 
+    def update_embed(self):
+        for _ in range(0, self.index + 1):
+            self.embed = create_embed(self.ctx, self.index)
+            if self.embed.description != '': break
+            self.index -= 1
+        
     @discord.ui.select(placeholder='Choose page...', min_values=0, row=0)
     async def select_callback(self, select, interaction):
         if self.ctx.voice_client == None: return
         value = int(select.values[0])
         self.index = value
-        self.embed = create_embed(self.ctx, value)
+        self.update_embed()
         self.update_buttons()
         await interaction.response.edit_message(embed=self.embed, view=self)
 
@@ -303,7 +320,7 @@ class music_view(discord.ui.View):
     async def super_backward_callback(self, button, interaction):
         if self.ctx.voice_client == None: return
         self.index = 0
-        self.embed = create_embed(self.ctx, self.index)
+        self.update_embed()
         self.update_buttons()
         await interaction.response.edit_message(embed=self.embed, view=self)
 
@@ -311,14 +328,14 @@ class music_view(discord.ui.View):
     async def backward_callback(self, button, interaction):
         if self.ctx.voice_client == None: return
         self.index -= 1
-        self.embed = create_embed(self.ctx, self.index)
+        self.update_embed()
         self.update_buttons()
         await interaction.response.edit_message(embed=self.embed, view=self)
 
     @discord.ui.button(label='REFRESH' ,emoji='🔄', style=discord.ButtonStyle.red, row=1)
     async def refresh_callback(self, button, interaction):
         if self.ctx.voice_client == None: return
-        self.embed = create_embed(self.ctx, self.index)
+        self.update_embed()
         self.update_buttons()
         await interaction.response.edit_message(embed=self.embed, view=self)
 
@@ -326,15 +343,15 @@ class music_view(discord.ui.View):
     async def forward_callback(self, button, interaction):
         if self.ctx.voice_client == None: return
         self.index += 1
-        self.embed = create_embed(self.ctx, self.index)
+        self.update_embed()
         self.update_buttons()
         await interaction.response.edit_message(embed=self.embed, view=self)
 
     @discord.ui.button(label='LAST PAGE' ,emoji='⏩', style=discord.ButtonStyle.red, row=1)
     async def super_forward_callback(self, button, interaction):
         if self.ctx.voice_client == None: return
-        self.index = math.ceil(len(playlist[self.server]) / 25) - 1
-        self.embed = create_embed(self.ctx, self.index)
+        self.index = math.ceil(len(playlist[self.server]) / 50) - 1
+        self.update_embed()
         self.update_buttons()
         await interaction.response.edit_message(embed=self.embed, view=self)
 
@@ -346,19 +363,20 @@ class music_view(discord.ui.View):
         await VoiceChat.stop(self.ctx)
         await asyncio.sleep(0.5)
         looping[self.server] = temp
-        self.embed = create_embed(self.ctx, self.index)
+        self.update_embed()
         self.update_buttons()
         await interaction.response.edit_message(embed=self.embed, view=self)
 
     @discord.ui.button(label='SHUFFLE' ,emoji='🔀', style=discord.ButtonStyle.red, row=2)
     async def shuffle_callback(self, button, interaction):
         if self.ctx.voice_client == None: return
-        if playlist[self.server] == []: return
-        temp = playlist[self.server][0]
-        playlist[self.server].pop(0)
-        np.random.shuffle(playlist[self.server])
-        playlist[self.server].insert(0, temp)
-        self.embed = create_embed(self.ctx, self.index)
+        if playlist[self.server][0] == []: return
+        temp = playlist[self.server][0][0]
+        playlist[self.server][0].pop(0)
+        np.random.shuffle(playlist[self.server][0])
+        np.random.shuffle(playlist[self.server][1])
+        playlist[self.server][0].insert(0, temp)
+        self.update_embed()
         self.update_buttons()
         await interaction.response.edit_message(embed=self.embed, view=self)
 
@@ -376,5 +394,6 @@ class music_view(discord.ui.View):
         else:
             await VoiceChat.resume(self.ctx)
         await interaction.response.edit_message(view=self)
+
 def setup(client, tokens):
     client.add_cog(music(client, tokens))
