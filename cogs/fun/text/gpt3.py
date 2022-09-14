49abc316d9e9dd7682021d00c2eab360d6f391a5
Salabombo <salabombo@outlook.com>
2022-09-14 12:30:40 +0300

    Update gpt3.py

--- cogs/fun/text/gpt3.py
+++ cogs/fun/text/gpt3.py
@@ -1,22 +1,8 @@
 from discord.ext import commands
 import discord
 import openai
-
+import functools
 from utility.common import decorators
-
-def CreateText(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt + ':',
-        temperature=0.5,
-        max_tokens=256,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-        )
-    text = response["choices"][0]["text"]
-    text = text.replace('\n\n', '\n')
-    return text
     
 class gpt3(commands.Cog):
     def __init__(self, bot, tokens):
@@ -25,14 +11,28 @@ class gpt3(commands.Cog):
         self.tokens = tokens
         openai.api_key = self.tokens[1]
 
-    @commands.command(aliases=["text", "ai"], help='prompt: the message to be sent to the ai')
+    def create_text(self, prompt):
+        response = openai.Completion.create(
+            engine='text-davinci-002',
+            prompt=prompt + ':',
+            temperature=0.5,
+            max_tokens=256,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+            )
+        text = response['choices'][0]['text']
+        text = text.replace('\n\n', '\n')
+        return text
+
+    @commands.command(aliases=['text', 'ai'], help='prompt: the message to be sent to the ai')
     @commands.is_nsfw()
     @commands.cooldown(1, 10, commands.BucketType.user)
     @decorators.typing
-    async def gpt3(self, ctx, *, prompt="make up a 4chan greentext post"):
+    async def gpt3(self, ctx, *, prompt='make up a 4chan greentext post'):
         embed = discord.Embed(color=0xC9EDBE, fields=[], title=prompt)   
         loop = ctx.bot.loop
-        text = await loop.run_in_executor(None, CreateText, prompt)
+        text = await loop.run_in_executor(None, functools.partial(self.create_text, prompt=prompt))
         embed.description = f'```{text[:4090]}```'
         await ctx.reply(embed=embed, mention_author=False)
 
