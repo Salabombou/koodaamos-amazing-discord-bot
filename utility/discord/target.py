a2ba148a81fef2d6324d0bfc610343072e494b23
Salabombo <salabombo@outlook.com>
2022-09-17 01:53:09 +0300

    better target fetcher

--- utility/discord/target.py
+++ utility/discord/target.py
@@ -1,57 +1,75 @@
 import discord
 from utility.common.errors import TargetNotFound
-# THE ORDER FOR THE FILES
-# 1.   Attachments
-# 2.   Embeds
-#   a. Video
-#   b. Thumbnail
-#   c. Image
 
-# THE ORDER WHERE TO LOOK FOR FILES
-# 1. From the replied message
-# 2. From the command message itself
-# 3. From the channel history
+"""
+THE ORDER FOR THE FILES
+    Sticker
+    Attachment
+    Embed
+        Video
+        Image
+        Thumbnail
 
-async def get_file(embeds, attachments, no_aud, no_vid, no_img): # gets the file for the img if there is one
-    for attachment in attachments:
-        if attachment.content_type != None:
-            content_type = attachment.content_type[0:5]
-            not_allowed = (content_type == 'audio' and no_aud) or (content_type == 'video' and no_vid) or (content_type == 'image' and no_img)
-            if not_allowed:
-                continue
-            return attachment
-    for embed in embeds:
-        if isinstance(embed,discord.embeds.Embed):
-            if isinstance(embed.video.proxy_url, str) and not no_vid:
-                return embed.video
-            if isinstance(embed.image.proxy_url, str) and not no_img:
-                return embed.image
-            if isinstance(embed.thumbnail.proxy_url, str) and not no_img:
-                return embed.thumbnail
-    return None
+THE ORDER WHERE TO LOOK FOR FILES
+    From the replied message
+    From the command message itself
+    From the channel history
+"""
 
-# Overlay a greenscreen video on top of the target.
+class target_fetcher:
+    def __init__(self, no_aud=False, no_vid=False, no_img=False) -> None:
+        self.aud = not no_aud
+        self.vid = not no_vid
+        self.img = not no_img
+
+        self.allowed = lambda c: (
+            c == 'audio' and self.aud) or (
+            c == 'video' and self.vid) or (
+            c == 'image' and self.img)
+
+    def get_file(self, embeds, attachments, stickers):
+        for sticker in stickers:
+            sticker.width = 320  # these attributes are missing from the StickerItem object
+            sticker.height = 320
+            sticker.proxy_url = sticker.url
+            return sticker
+        for attachment in attachments:
+            if attachment.content_type != None:
+                content_type = attachment.content_type[0:5]
+                if self.allowed(content_type):
+                    return attachment
+        for embed in embeds:
+            if isinstance(embed, discord.embeds.Embed):
+                if isinstance(embed.video.proxy_url, str) and self.vid:
+                    return embed.video
+                if isinstance(embed.image.proxy_url, str) and self.img:
+                    return embed.image
+                if isinstance(embed.thumbnail.proxy_url, str) and self.img:
+                    return embed.thumbnail
+        return None
+
 async def get_target(ctx, no_aud=False, no_vid=False, no_img=False):
-    history =  await ctx.channel.history(limit=50).flatten()
-    file = None
-
-    if ctx.message.reference != None: # if its a reply 
+    history = await ctx.channel.history(limit=50).flatten()
+    fetcher = target_fetcher(no_aud, no_vid, no_img)
+    stickers = ctx.message.stickers
+    attachments = ctx.message.attachments
+    file = fetcher.get_file([], attachments, stickers) # if there are embeds or attachments in the command itself
+    
+    for message in history[1:]: # first item in the list is likely the ctx
+        if file != None:
+            break
+        stickers = message.stickers
+        attachments = message.attachments
+        embeds = message.embeds
+        file = fetcher.get_file(embeds, attachments, stickers)
+    
+    if ctx.message.reference != None:  # if its a reply
         reply = await ctx.channel.fetch_message(ctx.message.reference.message_id)
         embeds = reply.embeds
         attachments = reply.attachments
-        file = await get_file(embeds, attachments, no_aud, no_vid, no_img)
-        if file == None:
-            raise TargetNotFound()
+        stickers = reply.stickers
+        file = fetcher.get_file(embeds, attachments, stickers)
+
+    if file != None:
         return file
-    else: # if there are embeds or attachments in the command itself
-        embeds = ctx.message.embeds
-        attachments = ctx.message.attachments
-        file = await get_file([], attachments, no_aud, no_vid, no_img)
-    if file == None:
-        for message in history[1:]:
-            embeds = message.embeds
-            attachments = message.attachments
-            file = await get_file(embeds, attachments, no_aud, no_vid, no_img)
-            if file != None:
-                break
-    return file
+    raise TargetNotFound()
