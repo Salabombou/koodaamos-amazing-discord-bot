883cee21f229477a41decc5d86104049613858b4
Salabombou <salabombo@outlook.com>
2022-12-16 00:31:06 +0200

    Update requests.py

--- utility/common/requests.py
+++ utility/common/requests.py
@@ -6,7 +6,7 @@ async def get_redirect_url(url: str) -> str:
     """
         Gets the redirect url from url
     """
-    async with httpx.AsyncClient(follow_redirects=True) as client:
+    async with httpx.AsyncClient(follow_redirects=True, max_redirects=10) as client:
         resp = await client.head(url)
         resp.raise_for_status()
     redirect_url = str(resp.url)
